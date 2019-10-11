ff830c6586be5e750108d09f680a89310730450b
onnes-sam <56313371+onnes-sam@users.noreply.github.com>
2019-10-11 17:15:15 -0400

    Committed based on pandas code

--- mba-data-analysis.py
+++ mba-data-analysis.py
@@ -1,5 +1,20 @@
 import pandas as pd
-from datetime import datetime
-retail_dtypes = {'InvoiceNo': 'int', 'StockCode': 'str', 'Description': 'str', 'Quantity': 'int', 'InvoiceDate': 'datetime', 'UnitPrice': 'float', 'CustomerID': 'int', 'Country': 'str'}
-file=pd.read_csv('dataset.csv', dtype=retail_dtypes, parse_dates=True)
-print(file)
+import numpy as np
+import matplotlib.pyplot as plt
+retail_dtypes = {'InvoiceNo': 'str', 'StockCode': 'str', 'Description': 'str', 'Quantity': 'str', 'InvoiceDate': 'str',
+                 'UnitPrice': 'str', 'CustomerID': 'str', 'Country': 'str'}
+mba_dataset=pd.read_csv('dataset.csv')
+pd.to_numeric(mba_dataset['UnitPrice'])
+pd.to_numeric(mba_dataset['CustomerID'])
+pd.to_numeric(mba_dataset['Quantity'])
+pd.to_datetime(mba_dataset['InvoiceDate'], format='%m%d%Y %mm%ss', errors='ignore')
+mba_dataset = mba_dataset[np.isfinite(mba_dataset['CustomerID'])]#removed rows with null customer ids
+mba_dataset.CustomerID = mba_dataset.CustomerID.astype(int)
+percentages = []
+for gen in list(mba_dataset["Country"].unique()):
+    xin = mba_dataset["InvoiceNo"][mba_dataset["Country"] == gen]
+    print(xin.value_counts())
+    p = round((xin.value_counts()[1] / xin.value_counts()[1].sum()) * 100, 2)
+    #percentages.append(p)
+    #print(gen, "(% to exit) : ", p)
+#print(mba_dataset)
