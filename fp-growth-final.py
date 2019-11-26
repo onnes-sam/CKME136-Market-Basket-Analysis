82f68a5194ae7cc6d868300383572b9a0b4408af
onnes-sam <56313371+onnes-sam@users.noreply.github.com>
2019-11-25 19:17:48 -0500

    Added fp-growth final python file

--- fp-growth-final.py
+++ fp-growth-final.py
@@ -1,32 +1,51 @@
 import sys, os, time, datetime
 from pyspark.mllib.fpm import FPGrowth
 from operator import add
-#from pyspark.sql import SparkSession
 from pyspark import SparkContext
-from pyspark.sql import SQLContext
+from pyspark.sql import HiveContext
 from pyspark.sql.functions import date_format
 from pyspark.sql.types import *
 from pyspark.sql.functions import unix_timestamp, from_unixtime
 import shutil
+from pyspark.sql.functions import collect_list, col
 #init_notebook_mode(connected=True)
 #spark = SparkSession.builder.appName("Python Spark SQL basic example").getOrCreate()
 #sc = spark.sparkContext
-sc = SparkContext("local", "Simple App")
-sqlContext = SQLContext(sc)
-df = sqlContext.read.load('/test_dev/mba-code/dataset.csv', format='csv', header='true', inferSchema='true',encoding='UTF-8')
-df.select("InvoiceNo", "StockCode", "Description", "Quantity", "InvoiceDate","InvoiceDateWS" ,date_format(from_unixtime(unix_timestamp('InvoiceDateWS', 'mm/dd/yyy')), 'EEEE').alias('weekday'),"CustomerID","Country").write.save("Invoices.parquet", format="parquet")
-parquetFile = spark.read.parquet("Invoices.parquet")
+#sc = SparkContext("local", "Simple App")
+sqlContext = HiveContext(sc)
+#df = sqlContext.read.load(source='com.databricks.spark.csv', path = 'hdfs:/user/cloudera/capstone/dataset.csv', format='csv', header='true', inferSchema='true',encoding='UTF-8')
+#df = sqlContext.read.format('com.databricks.spark.csv').options(header='true').load('file.csv')
+df = sc.textFile('hdfs:/user/cloudera/capstone/dataset.csv').map(lambda line: line.split(","))
+fields = [StructField(field_name, StringType(), True) for field_name in header] #get the types of header variable fields
+schema = StructType(fields)
+filter_data = df.filter(lambda row:row != header)
+SelectDf = sqlContext.createDataFrame(filter_data, schema=schema)
+SelectDf.registerTempTable("transactions")
+DescriptionGrp = sqlContext.sql("SELECT distinct InvoiceNo,Description FROM transactions group by InvoiceNo,Description")
+DescriptionGrp.show(5)
+#df.select("InvoiceNo", "StockCode", "Description", "Quantity", "InvoiceDate","CustomerID","Country").write.save("hdfs:/user/cloudera/capstone/Invoices.parquet", format="parquet")
+#DescriptionGrp = sqlContext.sql("SELECT distinct InvoiceNo,Description FROM parquet.`hdfs:/user/cloudera/capstone/Invoices.parquet` group by InvoiceNo,Description")
+transactions = DescriptionGrp.groupBy("InvoiceNo").agg(collect_list("Description").alias("desc")).rdd.map(lambda x: x.desc)
+transactionsrdd = sc.parallelize(transactions.collect(),10000)
+model = FPGrowth.train(transactionsrdd, 0.002, 10)
+result = model.freqItemsets()
+modelresultsDF = sqlContext.createDataFrame(result)
+modelresultsDF.registerTempTable("modelresults")
+MostOrdered = sqlContext.sql('select items, freq from modelresults where size(items) > 2 order by freq desc limit 20')
+#parquetFile = spark.read.parquet("Invoices.parquet")
 # Parquet files can also be used to create a temporary view and then used in SQL statements.
-parquetFile.createOrReplaceTempView("parquetFile")
-DescriptionGrp = spark.sql("SELECT distinct InvoiceNo,StockCode FROM parquetFile group by InvoiceNo,StockCode")
+#parquetFile.createOrReplaceTempView("parquetFile")
+#DescriptionGrp = sqlContext.sql("SELECT distinct InvoiceNo,StockCode FROM parquetFile group by InvoiceNo,StockCode")
 #print(DescriptionGrp.rdd.take(2))
-minSupport = 0.05 * DescriptionGrp.rdd.count()
-apr_tem=DescriptionGrp.rdd.map(lambda x: (x[0], list([x[1]]))).reduceByKey(lambda x,y: x + y)
-transactions = apr_tem.map(lambda x: (x[1]))
+#minSupport = 0.05 * DescriptionGrp.rdd.count()
+#apr_tem=DescriptionGrp.rdd.map(lambda x: (x[0], list([x[1]]))).reduceByKey(lambda x,y: x + y)
+#final_transactions = apr_tem.map(lambda x: (x[1]))
+#schema = StructType([StructField("test_123",ArrayType(StringType(),True),True)])
+#transactions_dataframe = sqlContext.createDataFrame(final_transactions,schema)
+#transactions = transactions_dataframe.map(lambda line: line.strip().split(','))
 #schema = StructType([StructField("id", StringType(), True),StructField("items", ArrayType(StringType()), True)])
 #transactions = spark.createDataFrame(apr_tem, schema)
 #print(transactions.show(2))
-model = FPGrowth.train(transactions, minSupport=0.2, numPartitions=10)
+model = FPGrowth.train(transactions, minSupport=0.02, numPartitions=10)
 result = model.freqItemsets().collect()
-for fi in result:
-    print(fi)
+for fi in result:print(fi)
--- fp-growth-mba.py
+++ fp-growth-mba.py
@@ -1,3 +1,12 @@
+import sys, os, time, datetime
+
+sparkhome = '/usr/hdp/2.6.5.0-292/spark2/'
+os.environ['SPARK_HOME'] = sparkhome
+os.environ['PYSPARK_PYTHON']="/usr/hdp/2.6.5.0-292/spark2/python"
+sys.path.append(sparkhome + '/python')
+sys.path.append(sparkhome + '/python/lib/py4j-0.10.6-src.zip')
+sys.path.append(sparkhome + '/python/lib/pyspark.zip')
+
 from pyspark.mllib.fpm import FPGrowth
 from operator import add
 from pyspark.sql import SparkSession
@@ -6,24 +15,44 @@ from pyspark.sql.functions import date_format
 from pyspark.sql.types import *
 import pandas as pd
 from pyspark.sql.functions import unix_timestamp, from_unixtime
-#import plotly.graph_objs as go
-#from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+from pyspark.sql.functions import concat_ws
+
+
 import shutil
 #init_notebook_mode(connected=True)
 spark = SparkSession.builder.appName("Python Spark SQL basic example").getOrCreate()
 sc = spark.sparkContext
 sqlContext = SQLContext(sc)
-df = sqlContext.read.load('/test_dev/mba-code/dataset.csv', format='csv', header='true', inferSchema='true')
+df = sqlContext.read.load('/test_dev/mba-code/dataset.csv', format='csv', header='true', inferSchema='true',encoding='UTF-8')
 df.select("InvoiceNo", "StockCode", "Description", "Quantity", "InvoiceDate","InvoiceDateWS" ,date_format(from_unixtime(unix_timestamp('InvoiceDateWS', 'mm/dd/yyy')), 'EEEE').alias('weekday'),"CustomerID","Country").write.save("Invoices.parquet", format="parquet")
 parquetFile = spark.read.parquet("Invoices.parquet")
 # Parquet files can also be used to create a temporary view and then used in SQL statements.
 parquetFile.createOrReplaceTempView("parquetFile")
 DescriptionGrp = spark.sql("SELECT distinct InvoiceNo,StockCode FROM parquetFile group by InvoiceNo,StockCode")
-print(DescriptionGrp.rdd.take(2))
+#print(DescriptionGrp.rdd.take(2))
 minSupport = 0.05 * DescriptionGrp.rdd.count()
-apr_tem=DescriptionGrp.rdd.map(lambda x: (x[0], [x[1]])).reduceByKey(lambda x,y: x + y)
-transactions = apr_tem.map(lambda x: (x[1]))
-model = FPGrowth.train(transactions, minSupport=0.2, numPartitions=10)
-result = model.freqItemsets().collect()
-for fi in result:
-    print(fi)
+apr_tem=DescriptionGrp.rdd.map(lambda x: (x[0], list([x[1]]))).reduceByKey(lambda x,y: x + y)
+schema = StructType([StructField("id", StringType(), True),StructField("items", ArrayType(StringType()), True)])
+transactions = spark.createDataFrame(apr_tem, schema)
+print(transactions.show(2))
+##transactions_fp=apr_tem.map(lambda x: (x[1]))
+#print(transactions_fp.take(2))
+#schema = StructType([StructField("test_123",ArrayType(StringType(),True),True)])
+#fields = [StructField(field_name, StringType(), True) for field_name in schema.split(',')]
+#schema = StructType(fields)
+##final_transactions_rdd = sc.parallelize(transactions_fp.collect())
+##final_transactions = final_transactions_rdd.map(lambda x : ','.join(x))
+##print(final_transactions.take(2))
+#transactions = spark.createDataFrame([final_transactions])
+##transactions = final_transactions.map(lambda line: line.strip().split(','))
+##print(transactions.take(2))
+fpgrowth = FPGrowth(itemsCol="items", minSupport=0.5, minConfidence=0.6)
+##fpgrowth = FPGrowth(minSupport=0.5, minConfidence=0.6)
+model = fpgrowth.fit(transactions)
+# Display frequent itemsets.
+model.freqItemsets.show()
+# Display generated association rules.
+model.associationRules.show()
+# transform examines the input items against all the association rules and summarize the
+# consequents as prediction
+model.transform(df).show()
